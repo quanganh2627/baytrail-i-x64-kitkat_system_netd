2b3e183d3cb0c062f76a6238fadbd28f248fdf5a
Benzi Zbit <benzi.zbit@intel.com>
2014-06-24 14:38:46 +0200

    iwl: fix control intraface and disable FW reload.
    Skip the FW switch, for iwl device.
    BRCM are using an other uCode for SoftAP, but we do not.

--- SoftapController.cpp
+++ SoftapController.cpp
@@ -137,7 +137,7 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
     }
 
     asprintf(&wbuf, "interface=%s\ndriver=nl80211\nctrl_interface="
-            "/data/misc/wifi/hostapd\nssid=%s\nchannel=%d\nieee80211n=1\n"
+            "/data/misc/wifi/sockets\nssid=%s\nchannel=%d\nieee80211n=1\n"
             "hw_mode=g\nignore_broadcast_ssid=%d\n",
             argv[2], argv[3], channel, hidden);
 
@@ -201,6 +201,10 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
  */
 int SoftapController::fwReloadSoftap(int argc, char *argv[])
 {
+#ifdef NO_FW_RELOAD_FOR_SOFTAP
+    ALOGD("Softap skip FW reload");
+    return 0;
+#else
     int i = 0;
     char *fwpath = NULL;
 
@@ -226,6 +230,7 @@ int SoftapController::fwReloadSoftap(int argc, char *argv[])
         ALOGD("Softap fwReload - Ok");
     }
     return ResponseCode::SoftapStatusResult;
+#endif
 }
 
 void SoftapController::generatePsk(char *ssid, char *passphrase, char *psk_str) {
